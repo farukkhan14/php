142e2e77202c56deb5c8a9f665ded9f883ec420d
farukkhan <faruk.cse14@gmail.com>
2015-05-28 14:47:59 +0600

    sort the number in ascendign order.

---
+++ Raw-PHP/sorting.php
@@ -0,0 +1,6 @@
+ <?php 
+ 	$number_array = [10,6,9,7,6];
+ 	$sort_in_ascending_order = sort($number_array);
+ 	//echo "$sort_in_ascending_order";
+  ?>
+  <pre><?php print_r($sort_in_ascending_order); ?></pre>
